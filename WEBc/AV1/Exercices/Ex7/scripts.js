d23e96331620bbf707e948625a6ee8e76ae7f0b2
sergio <sergiricartt@gmail.com>
2024-10-16 18:15:42 +0200

    wordle

--- WEBc/AV1/Exercices/Ex7/scripts.js
+++ WEBc/AV1/Exercices/Ex7/scripts.js
@@ -1,8 +1,89 @@
 
-let i = 0;
+let i = 1;
 
-function añadirLetra(e) {
-    const valor = e.target.textContent;
+
+function verificarTamanyo() {
+}
+
+
+function verificar() {
+    console.log("verificar");
+}
+
+
+function anyadirLetra(e) {
+    let valor = e.target.textContent;
+    let cuadro = document.getElementById("caja" + i);
+    if (i == 6 || i == 11 || i == 16 || i == 21 || i == 26 || i == 31) {
+        valor = 'Enter';
+
+    }
+
+    switch (valor) {
+        case 'A':
+        case 'B':
+        case 'C':
+        case 'D':
+        case 'E':
+        case 'F':
+        case 'G':
+        case 'H':
+        case 'I':
+        case 'J':
+        case 'K':
+        case 'L':
+        case 'M':
+        case 'N':
+        case 'O':
+        case 'P':
+        case 'Q':
+        case 'R':
+        case 'S':
+        case 'T':
+        case 'U':
+        case 'V':
+        case 'W':
+        case 'X':
+        case 'Y':
+        case 'Z':
+        case 'Ñ':
+            i++;
+            cuadro.textContent = valor;
+            break;
+        case '←':
+            i--;
+            if (i > 1) {
+                cuadro = document.getElementById("caja" + i);
+                cuadro.textContent = "";
+            }else{
+                cuadro.textContent = "";
+            }
+
+            break;
+        case 'Enter':
+            if (i == 6 || i == 11 || i == 16 || i == 21 || i == 26 || i == 31) {
+                document.querySelectorAll(".cajaLetra").forEach(cuadro => {
+                    cuadro.style.transition = "border-color 0.3s ease-in-out";
+                    cuadro.style.borderColor = "rgb(209, 209, 209)";
+                });
+                verificar();
+            } else {
+                const idFila = `fila${Math.ceil(i / 5)}`;
+                document.getElementById(idFila).querySelectorAll(".cajaLetra").forEach(cuadro => {
+                    cuadro.style.transition = "border-color 0.3s ease-in-out";
+                    cuadro.style.borderColor = "red";
+                });
+            }
+
+            i++
+            break;
+
+    };
+}
+
+/*
+function añadirLetraTeclado(e) {
+    const valor = e.key.toUpperCase();
     let cuadro = document.getElementById("caja" + i);
 
     switch (valor) {
@@ -32,29 +113,38 @@ function añadirLetra(e) {
         case 'X':
         case 'Y':
         case 'Z':
+        case 'Ñ':
             i++;
             cuadro.textContent = valor;
             break;
-        case '←':
+        case "BACKSPACE":
+            i--;
             if (i > 1) {
-                i--;
+                cuadro = document.getElementById("caja" + i);
                 cuadro.textContent = "";
             }else{
                 cuadro.textContent = "";
             }
+
+
+            break;
+        case "ENTER":
+            verificarTamanyo(i);
             break;
 
     };
-
 }
 
 
-
+*/
 
 //event listener 
 document.addEventListener('DOMContentLoaded', () => {
     // Aquí coloca tu código para agregar el event listener
     document.querySelectorAll('.tecla').forEach(tecla => {
-        tecla.addEventListener('click', añadirLetra);
+        tecla.addEventListener('click', anyadirLetra);
     });
+    //document.addEventListener('keydown', añadirLetraTeclado)
+
 });
+
