1d24b1164175ea872e137437ac587478f991d9f6
sergio <sergiricartt@gmail.com>
2024-10-28 16:28:42 +0100

    Cliente

--- WEBc/AV1/Exercices/Ex8/script.js
+++ WEBc/AV1/Exercices/Ex8/script.js
@@ -1,1 +1,24 @@
-let prueba = 0;
+let eleccion = "";
+
+//MODO DE JUEGO
+document.addEventListener("DOMContentLoaded", function() {
+    const botonesModoJuego = document.getElementsByClassName("modoJuego");
+    for (let boton of botonesModoJuego) {
+        boton.addEventListener("click", function(event) {
+            const target = event.target;
+            if (target.id === "todas") {
+                target.style.backgroundColor = "rgb(124, 0, 0)";
+                document.getElementById("soloColor").style.backgroundColor = "red";
+                eleccion = "todas";
+            }
+            else if (target.id === "soloColor") {
+                target.style.backgroundColor = "rgb(124, 0, 0)";
+                document.getElementById("todas").style.backgroundColor = "red";
+                eleccion = "soloColor";
+            }
+        });
+    }
+});
+
+
+
