fd438c03f016405e151ce01bbd967bede36c0251
sergio <sergiricartt@gmail.com>
2024-10-29 15:51:54 +0100

    cliente

--- WEBc/AV1/Exercices/Ex8/script.js
+++ WEBc/AV1/Exercices/Ex8/script.js
@@ -22,21 +22,36 @@ document.addEventListener("DOMContentLoaded", function() {
 
 
 
-let tiempo = document.getElementsByClassName("tiempo")
-console.log(tiempo)
-let finalizado = true
-let seconds = 0
-let minutes = 0
-setInterval(timerFunction, 1000);
-function timerFunction(){
-    seconds++
-    console.log(seconds)
+let segundos = 0;
+let minutos = 0;
 
-    if (seconds == 60){
-        seconds = 0
-        minutes+++
-        console.log("an")
-    }
-    tiempo.innerText = minutes + ":" + seconds
+function iniciarTemporizador() {
+    setInterval(() => {
+        segundos++;
+        if (segundos === 60) {
+            segundos = 0;
+            minutos++;
+
+        }
+        console.log(segundos);
+        // Formateo de tiempo
+        const tiempoTexto = `${minutos < 10 ? '0' : ''}${minutos}:${segundos < 10 ? '0' : ''}${segundos}`;
+
+        if (minutos < 10 && segundos < 10) {
+            document.getElementById("tiempo").textContent = "0" + minutos + ":0" + segundos;
+        }else if (minutos < 10) {
+            document.getElementById("tiempo").textContent = "0" + minutos + ":" + segundos;
+        }else if (segundos < 10) {
+            document.getElementById("tiempo").textContent = minutos + ":0" + segundos;
+        }else {
+            document.getElementById("tiempo").textContent = minutos + ":" + segundos;
+        }
+        
+        // Actualización del HTML
+        document.getElementById("tiempo").textContent = tiempoTexto;
+    }, 1000); // Actualización cada segundo
 }
+
+
+window.onload = iniciarTemporizador();
 
