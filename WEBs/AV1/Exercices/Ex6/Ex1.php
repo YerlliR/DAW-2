27b1851e83a210bd82999134b10e3dc60ef64015
sergio <sergiricartt@gmail.com>
2025-01-10 18:06:00 +0100

    cockies

---
+++ WEBs/AV1/Exercices/Ex6/Ex1.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * @author Sergio Ricart Alabau
+*/
+?>
+
+<!DOCTYPE html>
+    <html lang="es">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Document</title>
+    </head>
+    <body>
+        <form action="">
+            <label for="name">Tu Nombre:</label>
+            <br>
+            <input type="text" name="name" id="name" placeholder="Nombre">
+            <br>
+            <br>
+            <label for="saludo">Que quieres recibir ?</label>
+            <br>
+            <input type="radio" name="saludo" id="saludo1" value="saludo"> Saludo
+            <br>
+            <input type="radio" name="saludo" id="saludo2" value="despedida"> Despedida
+            <br>
+            <br>
+            <input type="submit" name="" id="">
+
+        </form>
+    </body>
+</html>
